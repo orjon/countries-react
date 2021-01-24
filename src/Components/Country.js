dbf1bbc90bfd9e8d72d36ecca2c536caed263d28
orjon <github@orjon.com>
2021-01-24 18:38:51 +0000

    Styling updated

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -3,20 +3,38 @@ import React from 'react';
 const Country = ({ country }) => {
   let countryNameUrl = country.name.replaceAll(' ', '&nbsp;');
   let countryCapitalUrl = country.capital.replaceAll(' ', '&nbsp;');
-  // let area = country.area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  let area = 'unavailable';
+  if (country.area !== null) {
+    area = country.area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  var languages = [];
+  country.languages.forEach((language) => {
+    languages.push(' ' + language.name);
+  });
+
+  var currencies = [];
+  country.currencies.forEach((currency) => {
+    if (currency.symbol !== null) {
+      currencies.push(' ' + currency.symbol + ' ' + currency.name);
+    } else {
+      currencies.push(' ' + currency.name);
+    }
+  });
+
+  var timezones = [];
+  country.timezones.forEach((timezone) => {
+    timezones.push(' ' + timezone);
+  });
 
   return (
     <div className='card country'>
       <div className='details'>
-        {/* <h3 className='row countryName'>
-        <a href={`https://en.wikipedia.org/wiki/Special:Search/${countryNameUrl}>${country.name}&nbsp`}</a>
-        (${country.alpha3Code})</h3>  */}
-
         <h3 className='row countryName'>
           <a
             href={`https://en.wikipedia.org/wiki/Special:Search/${countryNameUrl}`}
           >
-            {country.name}
+            {country.name}&nbsp;
           </a>
           ({country.alpha3Code})
         </h3>
@@ -46,70 +64,74 @@ const Country = ({ country }) => {
           <div className='labels'>
             <p>Area</p>
           </div>
-          <div className='data'>{/* <p>{area}</p> */}</div>
+          <div className='data'>
+            <p>{area} sq km</p>
+          </div>
         </div>
 
-        {/* 
+        <div className='row'>
+          <div className='labels'>
+            <p>Population</p>
+          </div>
+          <div className='data'>
+            <p>
+              {country.population
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </p>
+          </div>
+        </div>
 
+        <div className='row'>
+          <div className='labels'>
+            <p>Languages</p>
+          </div>
+          <div className='data'>
+            <p>
+              [{languages.length}]{languages}
+            </p>
+          </div>
+        </div>
 
+        <div className='row'>
+          <div className='labels'>
+            <p>Currencies</p>
+          </div>
+          <div className='data'>
+            <p>
+              [{currencies.length}]{currencies}
+            </p>
+          </div>
+        </div>
 
-      <div className='row'>
-        <div className='labels'>
-          <p>Population</p>
+        <div className='row'>
+          <div className='labels'>
+            <p>Timezones</p>
+          </div>
+          <div className='data'>
+            <p>
+              [{timezones.length}]{timezones}
+            </p>
+          </div>
         </div>
-        <div className='data'>
-          <p>${country.population
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</p>
-        </div>
-      </div>
 
-      <div className='row'>
-        <div className='labels'>
-          <p>Languages</p>
+        <div className='row'>
+          <div className='labels'>
+            <p>Domain</p>
+          </div>
+          <div className='data'>
+            <p>{country.topLevelDomain}</p>
+          </div>
         </div>
-        <div className='data'>
-          <p>[${languages.length}]${languages}</p>
-        </div>
-      </div>
 
-
-
-      <div className='row'>
-        <div className='labels'>
-          <p>Currencies</p>
+        <div className='row'>
+          <div className='labels'>
+            <p>Calling Code</p>
+          </div>
+          <div className='data'>
+            <p>+{country.callingCodes}</p>
+          </div>
         </div>
-        <div className='data'>
-          <p>[${currencies.length}]${currencies}</p>
-        </div>
-      </div>
-
-      <div className='row'>
-        <div className='labels'>
-          <p>Timezones</p>
-        </div>
-        <div className='data'>
-          <p>[${timezones.length}]${timezones}</p>
-        </div>
-      </div>
-
-      <div className='row'>
-        <div className='labels'>
-          <p>Domain</p>
-        </div>
-        <div className='data'>
-          <p>${country.topLevelDomain}</p>
-        </div>
-      </div>
-
-      <div className='row'>
-        <div className='labels'>
-          <p>Calling Code</p>
-        </div>
-        <div className='data'>
-          <p>+${country.callingCodes}</p>
-        </div>
-      </div> */}
       </div>
     </div>
   );
